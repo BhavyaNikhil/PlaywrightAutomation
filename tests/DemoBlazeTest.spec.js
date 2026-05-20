5cd80c9afc978e81c89f126cc312ad50facbe0a9
Bhavya <bhavyavaazire@gmail.com>
2026-05-20 15:04:33 +0530

    Final Commit

--- tests/DemoBlazeTest.spec.js
+++ tests/DemoBlazeTest.spec.js
@@ -42,6 +42,7 @@ test('Demo Blaze Tests', async({page,pages})=>{
         //await pages.homePage.selectProduct(productName);
     })
     
+    //commented these lines because of synchronization problems
     /*logger.info('Landing in Cart Page');
     await allure.step('Landing in Cart Page', async() => {
         await pages.cartPage.addToCart();
