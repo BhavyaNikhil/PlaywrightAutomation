da2c4b322f72e935e3dce99f76702eb7fb140e80
Bhavya <bhavyavaazire@gmail.com>
2026-05-22 15:15:02 +0530

    Test CI CD

--- tests/DemoBlazeTest.spec.js
+++ tests/DemoBlazeTest.spec.js
@@ -42,7 +42,7 @@ test('Demo Blaze Tests', async({page,pages})=>{
         //await pages.homePage.selectProduct(productName);
     })
     
-    //commented these lines because of synchronization problem
+    //commented these lines because of synchronization problems
     /*logger.info('Landing in Cart Page');
     await allure.step('Landing in Cart Page', async() => {
         await pages.cartPage.addToCart();
